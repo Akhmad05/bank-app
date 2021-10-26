f6c63aa8d121e5b337f2a537588c5670c16d4c39
Aha <ahmadvt@mail.ru>
2021-10-26 22:49:24 +0300

    added liquibase - ORM

---
+++ bank-credit/src/main/java/ru/developer/controllers/MainController.java
@@ -0,0 +1,26 @@
+package ru.developer.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import ru.developer.Service.PersonServiceImpl;
+import ru.developer.model.Person;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private PersonServiceImpl personServiceImpl;
+
+    @GetMapping(value = "/main")
+    public ModelAndView showUserInfo() {
+        ModelAndView mav = new ModelAndView("mainPage");
+        List<Person> allPerson = personServiceImpl.getAll();
+        mav.addObject("results", allPerson);
+        return mav;
+    }
+
+}
--- bank-credit/src/main/webapp/WEB-INF/views/index.jsp
+++ bank-credit/src/main/webapp/WEB-INF/views/index.jsp
@@ -10,7 +10,8 @@
 <head>
     <title>Home page</title>
 </head>
-    <div style="font-size:25px; color:blue">Welcome to home page!</div><br>
-    <a href="./main">Go to the main page</a>
+<div style="font-size:25px; color:blue">Welcome to home page!</div>
+<br>
+<a href="./main">Go to the main page</a>
 </body>
 </html>
